2d5320cd27935269008c34992b9529a363c1e82a
ayo <ayo@ciklum.com>
2020-09-04 15:46:23 +0500

    Integrated Angular and also Electro template- Completed Home component - Routes

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../../services/product.service";
+import {Router} from "@angular/router";
+import {ProductModelServer, ServerResponse} from "../../models/product.models";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  products : ProductModelServer[] = [];
+  constructor(private productService : ProductService, private router : Router) { }
+
+  ngOnInit() {
+    this.productService.getAllProducts().subscribe((prods : ServerResponse) => {
+      this.products = prods.products;
+    });
+  }
+
+  selectProduct(id : Number){
+    this.router.navigate(['/product',id]).then();
+  }
+
+}
+
